e7bfaef77efbd7a66ee86a272e1212886b638645
Roman Prod <roman.bugriy1988@gmail.com>
2021-10-15 14:47:18 +0300

    career works

--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './aboutme.css';
+import myFoto from './myfoto.jpg';
 
 export default function Me() {
   return (
         <div className='meWrapper'> 
             <div className='meFoto'>
-                <img src='myfoto.jpg'></img>
+                <img src={myFoto} alt='Это моя фотография' width='100' height='100' />
             </div>
             <div className='meText'>
                 <p>Всем привет. Вы находитесь на сайте обо мне</p>
--- src/components/career/career.js
+++ src/components/career/career.js
@@ -6,7 +6,7 @@ import Posts from '../posts/posts';
 export default function Career() {
   return (
       <div className="wrapper">
-              <h2>Моя карьера</h2>;
+              <h2>Моя карьера</h2>
         <Me />
         <Roles />
         <Posts />
