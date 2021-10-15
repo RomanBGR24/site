a6ee5eda6154a7fe45ee4623d891a5b6a66e611c
RomanBGR24 <62135606+RomanBGR24@users.noreply.github.com>
2021-10-15 09:44:15 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link, BrowserRouter, Route, Switch} from 'react-router-dom';
+import Career from './components/career/career';
+import Projects from './components/projects/projects';
+import Skills from './components/skills/skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header></header>
+    <div id='m'>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Моя карьера</Link></li>
+            <li><Link to="/projects">Мои проекты</Link></li>
+            <li><Link to="/skills">Моя квалификация</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/"><Career /></Route>
+          <Route path="/projects"><Projects /></Route>
+          <Route path="/skills"><Skills /></Route>
+        </Switch>      
+      </BrowserRouter>
     </div>
+    <footer></footer>
   );
 }
 
---
+++ src/components/career/career.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Career() {
+  return <h2>Моя карьера</h2>;
+}
